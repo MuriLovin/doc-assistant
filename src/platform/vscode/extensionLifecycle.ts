a78609ccc90733d5ec499c9c6b6f4c7793956d33
MuriLovin <mu.4dev@gmail.com>
2026-03-08 17:53:56 -0300

    refactor: change the lifecycle entry point to be more generic

--- src/platform/vscode/extensionLifecycle.ts
+++ src/platform/vscode/extensionLifecycle.ts
@@ -1,44 +1,14 @@
 import * as vscode from 'vscode';
-import { ConfigureOpenAIUseCase } from '../../modules/commit-message/application/configureOpenAI';
-import { GenerateCommitMessageUseCase } from '../../modules/commit-message/application/generateCommitMessage';
-import { GitCliDiffReader } from '../../modules/commit-message/infrastructure/gitCliDiffReader';
-import { OpenAiCommitMessageClient } from '../../modules/commit-message/infrastructure/openAiCommitMessageClient';
-import { createCommitMessageCommands } from '../../modules/commit-message/presentation/commands';
-import {
-	VsCodeCommitInputGateway,
-	VsCodeSettingsGateway,
-	VsCodeUiGateway,
-	VsCodeWorkspaceGateway,
-} from './adapters';
+import { ExtensionModule } from '../../core/contracts/extensionModule';
+import { ExtensionModuleFactory } from '../../core/contracts/extensionModuleFactory';
+import { createExtensionModuleFactories } from '../../modules';
 
 export function activate(context: vscode.ExtensionContext): void {
-	const ui = new VsCodeUiGateway();
-	const settings = new VsCodeSettingsGateway(context);
-	const workspace = new VsCodeWorkspaceGateway();
-	const commitInput = new VsCodeCommitInputGateway();
-	const gitDiffReader = new GitCliDiffReader();
-	const aiClient = new OpenAiCommitMessageClient();
-
-	const configureOpenAIUseCase = new ConfigureOpenAIUseCase(settings, ui);
-	const generateCommitMessageUseCase = new GenerateCommitMessageUseCase(
-		settings,
-		workspace,
-		gitDiffReader,
-		aiClient,
-		commitInput,
-		ui,
-		configureOpenAIUseCase,
-	);
-
-	const commands = createCommitMessageCommands(generateCommitMessageUseCase, configureOpenAIUseCase, ui);
-
-	context.subscriptions.push(
-		vscode.commands.registerCommand(
-			'doc-assistant.generateCommitMessage',
-			commands.generateCommitMessage,
-		),
-		vscode.commands.registerCommand('doc-assistant.configureOpenAI', commands.configureOpenAI),
-	);
+	const moduleFactories: ExtensionModuleFactory[] = createExtensionModuleFactories(context);
+	const modules: ExtensionModule[] = moduleFactories.map((factory) => factory.create());
+	for (const module of modules) {
+		module.activate();
+	}
 }
 
 export function deactivate(): void {}
